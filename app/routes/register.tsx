2789330126bb5a9239ae2ea3dce2089ecfb9954c
ubotweb <ubot.web.id@gmail.com>
2026-07-21 18:26:39 +0700

    Create register.tsx

---
+++ app/routes/register.tsx
@@ -0,0 +1,16 @@
+import RegisterForm from '../islands/RegisterForm'
+
+export default function Register() {
+  return (
+    <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Daftar Member Baru</h2>
+      </div>
+      <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <RegisterForm />
+        </div>
+      </div>
+    </div>
+  )
+}
