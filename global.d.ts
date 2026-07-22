3e153f1775bae3f7f0314a812bec2ae506bb6bb6
ubotweb <ubot.web.id@gmail.com>
2026-07-22 10:04:35 +0700

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,9 @@ type Env = {
   SESSION_KV: KVNamespace
   BONUS_QUEUE: Queue
   JWT_SECRET: string
+  CLOUDINARY_CLOUD_NAME: string
+  CLOUDINARY_API_KEY: string
+  CLOUDINARY_API_SECRET: string
 }
 
 declare module 'hono' {
